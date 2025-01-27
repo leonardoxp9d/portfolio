e47093e8f436a9aef6e90097b703c5031694d4dc
Leonardo Soares <leonardoxp9d@gmail.com>
2025-01-27 05:53:21 -0300

    feat: Add project items, update themes and style-switcher, and optimize profile image

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -32,16 +32,16 @@ function setActiveStyle(color) {
 const dayNight = document.querySelector(".day-night");
 
 dayNight.addEventListener("click", () => {
-    dayNight.querySelector("i").classList.toggle("fa-sun");
     dayNight.querySelector("i").classList.toggle("fa-moon");
-    document.body.classList.toggle("dark");
+    dayNight.querySelector("i").classList.toggle("fa-sun");
+    document.body.classList.toggle("light");
 })
 
 window.addEventListener("load", () => {
-    if(document.body.classList.contains("dark")) {
-        dayNight.querySelector("i").classList.add("fa-sun");
-    } else { 
+    if(document.body.classList.contains("light")) {
         dayNight.querySelector("i").classList.add("fa-moon");
+    } else { 
+        dayNight.querySelector("i").classList.add("fa-sun");
 
     }
 });
